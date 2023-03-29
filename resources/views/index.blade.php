19e13382c317dd8340e1b7b3b1be1cac50307049
gabigsc <gabrielagsc2005@gmail.com>
2023-03-29 09:06:07 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta nome="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+    <h1> Conexão com o Banco de Dados Estabelecida </h1>
+</body>
+</html>
